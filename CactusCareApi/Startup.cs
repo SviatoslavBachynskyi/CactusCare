a9e211291b7c71fae526432d482c9f1f5cae7446
AntonVovk <archwolf@protonmail.com>
2019-12-01 22:35:19 +0200

    [feat] Added swagger support

--- CactusCareApi/Startup.cs
+++ CactusCareApi/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace CactusCareApi
 {
@@ -33,12 +34,20 @@ namespace CactusCareApi
             });
 
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "CactusCare API", Version = "v1"});
+            });
+
             new ConfigureBLL().ConfigureServices(services, Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "CactusCare API V1"); });
+
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
             app.UseCors(MyAllowSpecificOrigins);
