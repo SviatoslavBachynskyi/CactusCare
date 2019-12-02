0ab17b6f4a43a708517aa9e7d76410b4c8598ae4
AntonVovk <archwolf@protonmail.com>
2019-12-02 11:27:41 +0200

    [feat] Added IHospitalRepository

---
+++ CactusCare.Abstractions/Repositories/IHospitalRepository.cs
@@ -0,0 +1,11 @@
+﻿using CactusCare.Abstractions.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CactusCare.Abstractions.Repositories
+{
+    public interface IHospitalRepository : IBaseRepository<Hospital, int>
+    {
+    }
+}
