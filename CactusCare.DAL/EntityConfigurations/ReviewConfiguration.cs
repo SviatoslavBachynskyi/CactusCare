2f8bad2e7d51e8c195f0877cda9a0b682a502bc0
AntonVovk <archwolf@protonmail.com>
2019-12-02 22:16:36 +0200

    [feat] Added ReviewConfiguration

---
+++ CactusCare.DAL/EntityConfigurations/ReviewConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using CactusCare.Abstractions.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CactusCare.DAL.EntityConfigurations
+{
+    internal class ReviewConfiguration : IEntityTypeConfiguration<Review>
+    {
+        public void Configure(EntityTypeBuilder<Review> builder)
+        {
+        }
+    }
+}
