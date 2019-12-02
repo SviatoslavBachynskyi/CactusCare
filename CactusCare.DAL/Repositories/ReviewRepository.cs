9862135f6539a47035eb11a885b4e18660c03088
AntonVovk <archwolf@protonmail.com>
2019-12-02 22:22:29 +0200

    [feat] Added ReviewRepository implementation

---
+++ CactusCare.DAL/Repositories/ReviewRepository.cs
@@ -0,0 +1,12 @@
+﻿using CactusCare.Abstractions.Entities;
+using CactusCare.Abstractions.Repositories;
+
+namespace CactusCare.DAL.Repositories
+{
+    internal class ReviewRepository : BaseRepository<Review, int>, IReviewRepository
+    {
+        public ReviewRepository(CactusCareContext context) : base(context)
+        {
+        }
+    }
+}
