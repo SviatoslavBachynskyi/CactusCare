3d01eb67c79304ca8175143ac38f3d73ae685d2f
Sviatoslav Bachynskyi <sbach@softserveinc.com>
2019-12-02 18:10:49 +0200

    deleted workaround with developer scope

--- CactusCareApi/Startup.cs
+++ CactusCareApi/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using CactusCare.BLL;
 using CactusCare.DAL;
 using Microsoft.AspNetCore.Builder;
@@ -13,13 +14,16 @@ namespace CactusCareApi
     public class Startup
     {
         private const string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
-        public Startup(IConfiguration configuration)
+        
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
-            Configuration = configuration;
+            _configuration = configuration;
+            _environment = environment;
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration _configuration;
+
+        public IWebHostEnvironment _environment;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -30,7 +34,7 @@ namespace CactusCareApi
                     builder =>
                     {
                         // This policy will change on Azure.
-                        builder.WithOrigins(Configuration.GetSection("AllowedOrigins").ToString())
+                        builder.WithOrigins(_configuration.GetSection("AllowedOrigins").ToString())
                             .AllowAnyOrigin()
                             .AllowAnyHeader();
                     });
@@ -42,20 +46,18 @@ namespace CactusCareApi
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "CactusCare API", Version = "v1"});
             });
 
-            new ConfigureBLL().ConfigureServices(services, Configuration);
+            new ConfigureBLL().ConfigureServices(services, _configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IServiceProvider serviceProvider)
         {
             app.UseSwagger();
             app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "CactusCare API V1"); });
 
-            if (env.IsDevelopment())
+            if (_environment.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                var scope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope();
-                scope.ServiceProvider.GetRequiredService<CactusCareContext>().Database.Migrate();
+                serviceProvider.GetRequiredService<CactusCareContext>().Database.Migrate();
             }
 
             app.UseCors(MyAllowSpecificOrigins);
