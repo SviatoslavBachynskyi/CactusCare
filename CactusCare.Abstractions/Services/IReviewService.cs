65a1c0addcf34615ebb8a6dbc89f1d1544f2c90f
AntonVovk <archwolf@protonmail.com>
2019-12-02 21:37:12 +0200

    [feat] Added IReviewService

---
+++ CactusCare.Abstractions/Services/IReviewService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using CactusCare.Abstractions.DTOs;
+
+namespace CactusCare.Abstractions.Services
+{
+    public interface IReviewService
+    {
+        List<ReviewDTO> GetAll();
+
+        ReviewDTO Get(int id);
+    }
+}
