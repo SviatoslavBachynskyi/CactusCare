5300a690037cb4b534b2a553a1855791e5900cec
AntonVovk <archwolf@protonmail.com>
2019-12-02 22:09:13 +0200

    [feat] Added ReviewService implementation

---
+++ CactusCare.BLL/Services/ReviewService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using CactusCare.Abstractions;
+using CactusCare.Abstractions.DTOs;
+using CactusCare.Abstractions.Entities;
+using CactusCare.Abstractions.Services;
+
+namespace CactusCare.BLL.Services
+{
+    internal class ReviewService : IReviewService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public ReviewService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public ReviewDTO Get(int id)
+        {
+            var model = _unitOfWork.ReviewRepository.GetById(id);
+            return _mapper.Map<Review, ReviewDTO>(model);
+        }
+
+        public List<ReviewDTO> GetAll()
+        {
+            return _unitOfWork.ReviewRepository.GetAll().Select(s => _mapper.Map<Review, ReviewDTO>(s)).ToList();
+        }
+    }
+}
