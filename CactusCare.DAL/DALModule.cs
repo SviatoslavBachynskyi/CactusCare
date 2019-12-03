e587bdd54143b65fbbbd975592734a2be5e84e4f
Sviatoslav Bachynskyi <sbach@softserveinc.com>
2019-12-03 13:07:13 +0200

    used autofac for dependency injection of services and repositories

---
+++ CactusCare.DAL/DALModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Autofac;
+using CactusCare.Abstractions;
+using CactusCare.Abstractions.Repositories;
+using CactusCare.DAL.Repositories;
+using Microsoft.Extensions.Configuration;
+using Module = Autofac.Module;
+
+namespace CactusCare.DAL
+{
+    public class DALModule : Module
+    {
+        private IConfigurationRoot _configuration;
+
+        public DALModule(IConfigurationRoot configuration)
+        {
+            this._configuration = configuration;
+        }
+        protected override void Load(ContainerBuilder builder)
+        {
+            //register all repositories
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces();
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+        }
+    }
+}
