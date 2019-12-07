40e268fb443ee93e8a6b62ac97989dd86102106e
Anton Vovk <archwolf@protonmail.com>
2019-12-08 01:30:55 +0200

    [imp] Updated RatingConverter

--- CactusCare.BLL/Converters/RatingConverter.cs
+++ CactusCare.BLL/Converters/RatingConverter.cs
@@ -9,25 +9,25 @@
                 case 1:
                     return 0.5f;
                 case 2:
-                    return 1.0f;
+                    return 1;
                 case 3:
                     return 1.5f;
                 case 4:
-                    return 2.0f;
+                    return 2;
                 case 5:
                     return 2.5f;
                 case 6:
-                    return 3.0f;
+                    return 3;
                 case 7:
                     return 3.5f;
                 case 8:
-                    return 4.0f;
+                    return 4;
                 case 9:
                     return 4.5f;
                 case 10:
-                    return 5.0f;
+                    return 5;
                 default:
-                    return 0.0f;
+                    return 0;
             }
         }
     }
