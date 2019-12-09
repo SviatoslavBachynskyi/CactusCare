2eeba49ccf982bacadb1215305e663b4fc5f4be3
Sviatoslav Bachynskyi <sbach@softserveinc.com>
2019-12-09 19:38:52 +0200

    added migration

--- CactusCare.DAL/Migrations/20191209173516_CactusCareDB.Designer.cs
+++ CactusCare.DAL/Migrations/20191209173516_CactusCareDB.Designer.cs
@@ -4,14 +4,16 @@ using CactusCare.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CactusCare.DAL.Migrations
 {
     [DbContext(typeof(CactusCareContext))]
-    partial class CactusCareContextModelSnapshot : ModelSnapshot
+    [Migration("20191209173516_CactusCareDB")]
+    partial class CactusCareDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CactusCare.DAL/Migrations/20191209173516_CactusCareDB.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CactusCare.DAL.Migrations
+{
+    public partial class CactusCareDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Time",
+                value: new DateTime(2019, 12, 9, 19, 35, 16, 415, DateTimeKind.Local).AddTicks(2349));
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Time",
+                value: new DateTime(2019, 12, 9, 19, 35, 16, 420, DateTimeKind.Local).AddTicks(1488));
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Time",
+                value: new DateTime(2019, 12, 9, 19, 35, 16, 420, DateTimeKind.Local).AddTicks(1554));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Time",
+                value: new DateTime(2019, 12, 9, 19, 31, 33, 775, DateTimeKind.Local).AddTicks(5012));
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Time",
+                value: new DateTime(2019, 12, 9, 19, 31, 33, 780, DateTimeKind.Local).AddTicks(4845));
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Time",
+                value: new DateTime(2019, 12, 9, 19, 31, 33, 780, DateTimeKind.Local).AddTicks(4909));
+        }
+    }
+}
