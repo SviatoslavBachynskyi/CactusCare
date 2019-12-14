e1a12261cd68540b2e3dba0c36a9625800dd3fd2
Anton Vovk <archwolf@protonmail.com>
2019-12-14 17:31:20 +0200

    Added ValidationService

---
+++ CactusCareApi/CactusCare.BLL/Services/FluentValidationService.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using CactusCare.Abstractions.Services;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace CactusCare.BLL.Services
+{
+    public class FluentValidationService : IValidationService
+    {
+        private readonly IValidatorFactory _validatorFactory;
+
+        public FluentValidationService(IValidatorFactory validatorFactory)
+        {
+            this._validatorFactory = validatorFactory;
+        }
+
+        public Task<ValidationResult> ValidateAsync<T>(T entity) where T : class
+        {
+            var validator = this._validatorFactory.GetValidator(entity.GetType());
+            return validator.ValidateAsync(entity);
+        }
+    }
+}
